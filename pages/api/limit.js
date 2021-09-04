39a1029e6707e298b80f04c514a0eddb7c6690b7
Punith <punith@pop-os.localdomain>
2021-09-04 22:28:54 +0530

    api condition

--- pages/api/limit.js
+++ pages/api/limit.js
@@ -1,16 +1,21 @@
 import { check } from "../../src/bucket";
 
 export default async function handler(req, res) {
-  const { allowed, remaining } = await check("dheera");
-  if (!allowed) {
-    return res.json({
-      message: "Not allowed to send requests",
-      allowed,
-      remaining,
-    });
-  }
+  if (req.method === "GET") {
+    const { allowed, remaining } = await check("dheera");
+    if (!allowed) {
+      return res.json({
+        message: "Not allowed to send requests",
+        allowed,
+        remaining,
+      });
+    }
 
-  res
-    .status(200)
-    .json({ message: "You are allowed to send request", allowed, remaining });
+    res
+      .status(200)
+      .json({ message: "You are allowed to send request", allowed, remaining });
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).json({ message: `${req.method} is not allowed` });
+  }
 }
